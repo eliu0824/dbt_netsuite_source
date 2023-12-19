bdfd585d9d737f5dae1d5d8c6c3ab97bb70ac738
Justin Smethers <justin.smethers@gmail.com>
2023-12-19 11:46:55 -0500

    Added netsuite2__using_multi_caledar flag to sql files

--- models/netsuite2/stg_netsuite2__accounting_period_fiscal_cal.sql
+++ models/netsuite2/stg_netsuite2__accounting_period_fiscal_cal.sql
@@ -1,4 +1,4 @@
-{{ config(enabled=var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2')) }}
+{{ config(enabled=(var('netsuite_data_model', 'netsuite') == var('netsuite_data_model_override','netsuite2') and var('netsuite2_using_multi_calendar', true))) }}
 
 with base as (
 
